5eb3f3a3c04749376bc7b48ada5940bb375ccfaa
maysing <kjma21sc@student.ju.se>
2022-10-03 17:55:37 +0200

    Implemented database

--- data.js
+++
@@ -1,7 +0,0 @@
-exports.works = [{
-    id: 1, 
-    title: "Art", 
-}, {
-    id: 2, 
-    title: "Photo"
-}]
--- main.js
+++ main.js
@@ -4,8 +4,19 @@ const data = require('./data.js')
 const sqLite3 = require ('sqLite3')
 const db = new sqLite3.Database("mayaKjellenPortfolioDb.db")
 
- 
+db.run(`
+
+    CREATE TABLE IF NOT EXISTS works(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        title TEXT,
+        link TEXT,
+        course TEXT, 
+        year INTEGER
+    )
+`)
+
 const app = express()
+
  
 app.engine('hbs', expressHandlebars.engine({
     defaultLayout: 'main.hbs',
@@ -38,41 +49,57 @@ app.post('/admin', function(request, response){
     const course = request.body.course
     const year = request.body.year
 
-    data.works.push({
-        id: data.works.at(-1).id +1,
-        title: title, 
-        link: link, 
-        course: course, 
-        year: year
+    const query = "INSERT INTO works (title, link, course, year) VALUES (?, ?, ?, ?)"
+    const values = [title, link, course, year]
 
+    db.run(query, values, function(error){
+        if(error){
+            console.log(error)
+        } else {
+           response.redirect('/works')  
+        }
     })
-
-    response.redirect('/works')
-
 })
-
 
 app.get('/works', function(request, response){
 
-    const model = {
-        works: data.works
-    }
+    const query = "SELECT * FROM works ORDER BY id"
 
-    response.render('works.hbs', model)
+    db.all(query, function(error, works) {
+
+        if(error) {
+            console.log(error)
+        } else {
+            const model = {
+              works
+            }
+
+        response.render('works.hbs', model) 
+        }
+   
+    })
 
 })
+
 
 app.get("/works/:id", function(request, response){
 	
 	const id = request.params.id
 	
-	const work = data.works.find(m => m.id == id)
-	
-	const model = {
+	const query = "SELECT * FROM works WHERE id = ?"
+    const values = [id]
+
+    db.get(query, values, function(error, work){
+        if(error){
+            console.log(error)
+        }else{const model = {
 		work: work,
 	}
 	
 	response.render('work.hbs', model)
+            
+        }
+    })
 	
 })
 
