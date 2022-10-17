8e8ec9778461199bfa05eb70eb2f0dee3d9018b6
maysing <kjma21sc@student.ju.se>
2022-10-17 16:45:36 +0200

    Delete Guest

--- main.js
+++ main.js
@@ -3,7 +3,6 @@ const expressHandlebars = require('express-handlebars')
 const sqLite3 = require ('sqLite3')
 const db = new sqLite3.Database("mayaKjellenPortfolioDb.db")
 const expressSession = require('express-session')
-
 const fieldEmpty = 0
 const app = express()
 
@@ -13,7 +12,6 @@ const adminUsername = "maya"
 const adminPassword = "password1"
 
 
-
 app.use(
 	expressSession({
 		saveUninitialized: false,
@@ -21,38 +19,25 @@ app.use(
 		secret: "fsecufeoue"
 	})
 )
-
-
-
 app.use(
 	function(request, response, next){
 		response.locals.session = request.session
 		next()
 	}
 )
-
-
 app.engine('hbs', expressHandlebars.engine({
     defaultLayout: 'main.hbs',
 }))
-
-
 app.use(
     express.static('public')
 )
-
-
 app.use(
 	express.urlencoded({
 		extended: false
 	})
 )
-
-
 //Create databases
-
 db.run(`
-
     CREATE TABLE IF NOT EXISTS works(
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         title TEXT,
@@ -61,36 +46,24 @@ db.run(`
         year INTEGER
     )
 `)
-
-
 db.run(`
-
     CREATE TABLE IF NOT EXISTS guests(
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         name TEXT
     )
 `)
-
-
 db.run(`
-
     CREATE TABLE IF NOT EXISTS comments(
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         comment TEXT
     )
 `)
 
-
 //Start Page
-
 app.get('/', function(request, response){
-
     const query = "SELECT * FROM guests ORDER BY id"
-
     const guestInputErrors = []
-
     db.all(query, function(error, guests) {
-
         if(error) {
             guestInputErrors.push("Internal error")
             
@@ -107,68 +80,92 @@ app.get('/', function(request, response){
 
 
 app.post('/', function(request, response){
-
-    const name = request.body.name
-
-    const guestInputErrors = []
-
-    if(name.length == fieldEmpty){
-        guestInputErrors.push("Title field can not be empty")
-    }
-
-    if(guestInputErrors.length == 0){
+    const name = request.body.name    
+        
         const query = "INSERT INTO guests (name) VALUES (?)"
         const values = [name]
 
         db.run(query, values, function(error){
-            if(error){
-                guestInputErrors.push("Internal error")
-            }
-
-                const model = {
-                    guestInputErrors
-                }
-
-
-            response.render('start.hbs', model)
-            
+            response.redirect("/")
         })
-    }
 })
+
+app.post("/delete-guest/:id", function(request, response){
+
+
+    const id = request.params.id
+    
+    const query = `DELETE FROM guests WHERE id = ?`
+    
+    db.run(query, id, function(error){
+    
+        response.redirect("/")
+    
+    })
+    
+    })
+
+
+    app.get("/update-guest/:id", function(request, response){
+
+        const id = request.params.id
+    
+        const query = "SELECT * FROM guests WHERE id = ?"
+    
+        const values = [id]
+    
+        db.get(query, values, function(error, guests){
+    
+        const model = {
+        guests
+    }
+        response.render("updateGuest.hbs", model)
+    })
+    
+    })
+    
+    
+    
+    app.post('/update-guest/:id', function(request, response){
+    
+        const id = request.params.id
+        const name = request.body.name
+    
+        const query = `UPDATE guests SET name = ?`
+        const values = [name, id]
+    
+            db.run(query, values, function(error){
+    
+                response.redirect('/')    
+            })
+    })
+
+
+    
 
 
 //About Page
-
 app.get('/about', function(request, response){   
     response.render('about.hbs')
 })
-
 //Contact Page
-
 app.get('/contact', function(request, response){   
     response.render('contact.hbs')
 })
-
 //Administration/Posting Page
-
 app.get('/admin', function(request, response){
     response.render('admin.hbs')
 })
-
 app.post('/admin', function(request, response){
     
     const title = request.body.title
     const link = request.body.link
     const course = request.body.course
     const year = request.body.year
-
-
     const inputErrors = []
-
     if(title.length == fieldEmpty){
         inputErrors.push("Title field can not be empty")
     }
-
     if(year.length == fieldEmpty){
         inputErrors.push("Year field can not be empty")
     }
@@ -179,12 +176,9 @@ app.post('/admin', function(request, response){
     } else if(year < 0){
         inputErrors.push("The year can not be negative")
     }
-
-
     if(inputErrors.length == 0){
         const query = "INSERT INTO works (title, link, course, year) VALUES (?, ?, ?, ?)"
         const values = [title, link, course, year]
-
         db.run(query, values, function(error){
             if(error){
                 inputErrors.push("Internal error")
@@ -192,31 +186,21 @@ app.post('/admin', function(request, response){
             response.redirect('/works')  
             }
         })
-
     } else {
         const model = {
             inputErrors
         }
-
         if(!request.session.isLoggedIn){
             inputErrors.push("Not logged in")
         }
-
         response.render('admin.hbs', model)
     }
 })
-
-
 //Works Page
-
 app.get('/works', function(request, response){
-
     const query = "SELECT * FROM works ORDER BY id"
-
     const inputErrors = []
-
     db.all(query, function(error, works) {
-
         if(error) {
             inputErrors.push("Internal error")
             
@@ -224,24 +208,18 @@ app.get('/works', function(request, response){
             const model = {
               works
             }
-
         response.render('works.hbs', model) 
         }
    
     })
-
 })
-
-
 //Individual Work Page
-
 app.get("/works/:id", function(request, response){
 	
 	const id = request.params.id
 	
 	const query = "SELECT * FROM works WHERE id = ?"
     const values = [id]
-
     db.get(query, values, function(error, work){
         if(error){
             inputErrors.push("Internal error")
@@ -249,9 +227,9 @@ app.get("/works/:id", function(request, response){
         }else{const model = {
 		work: work,
 	}
-	
+
 	response.render('work.hbs', model)
-            
+
         }
     })
 })
@@ -279,7 +257,7 @@ db.run(query, id, function(error){
 
 
 app.get("/update-work/:id", function(request, response){
-    
+
     const id = request.params.id
 
     const query = "SELECT * FROM works WHERE id = ?"
@@ -295,13 +273,11 @@ app.get("/update-work/:id", function(request, response){
 })
 
 })
-    
-
 
 
 
 app.post('/update-work/:id', function(request, response){
-    
+
     const id = request.params.id
 
     const title = request.body.title
@@ -318,27 +294,14 @@ app.post('/update-work/:id', function(request, response){
     if(inputErrors.length == 0){
 
         db.run(query, values, function(error){
-     
+
             response.redirect('/works')    
         })
     }
 })
 
 
-// app.post("/update-work/:id", function(request, response){
 
-//     const id = request.params.id
-//     const values = [id, title, link, course, year]
-    
-//     const query = `UPDATE works SET title = ?, link = ?, course = ?, year = ? WHERE id = ?`
-    
-//     db.run(query, values, function(error){
-    
-//         response.render("updateWork.hbs")
-    
-//     })
-    
-//     })
 
 
 //Log in Page
@@ -346,9 +309,7 @@ app.post('/update-work/:id', function(request, response){
 app.get('/login', function(request, response){
     response.render('login.hbs')
 })
-
 app.post("/login", function(request, response){
-
 	const password = request.body.password
 	const username = request.body.username
 	
@@ -365,11 +326,9 @@ app.post("/login", function(request, response){
 		const model = {
 			failedToLogin: true
 		}
-
 		
 		response.render('login.hbs', model)
 	}
 })
-
  
 app.listen(8080)
